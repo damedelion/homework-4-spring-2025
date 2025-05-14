42428a8e60193bb928ff3a4d3d459bcec8d8b18d
Tatiana Bocharova <tanka.bocharova.23@gmail.com>
2025-05-14 18:50:01 +0300

    synt err fix

--- hw/code/conftest.py
+++ hw/code/conftest.py
@@ -34,6 +34,7 @@ def config(request):
         'vnc': vnc,
     }
 
+
 def pytest_configure(config):
     config.addinivalue_line(
         "markers", "needs_audience: mark test as requiring pre-created audience"
